b8024d0c610f1881bc5866bafadc054b7e2dcdf2
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-13 21:21:50 +0700

    added animation in button

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -2,11 +2,51 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden: {
+    x: "100vw",
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
+
+const nextVariants = {
+  hidden: {
+    x: "-100vw",
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+    },
+  },
+};
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
-    <motion.div className="base container" initial={{ x: "-100vw" }} animate={{ x: 0 }} transition={{ type: "spring", delay: 0.5 }}>
+    <motion.div className="base container" variants={containerVariants} initial="hidden" animate="visible">
       <h3>Step 1: Choose Your Base</h3>
       <ul>
         {bases.map((base) => {
@@ -20,9 +60,11 @@ const Base = ({ addBase, pizza }) => {
       </ul>
 
       {pizza.base && (
-        <motion.div className="next" initial={{ x: "-100vw" }} animate={{ x: 0 }} transition={{ type: "spring", stiffness: 120 }}>
+        <motion.div className="next" variants={nextVariants}>
           <Link to="/toppings">
-            <motion.button whileHover={{ scale: 1.1, textShadow: "0px 0px 8px rgb(255,255,255)", boxShadow: "0px 0px 8px rgb(255,255,255)" }}>Next</motion.button>
+            <motion.button variants={buttonVariants} whileHover="hover">
+              Next
+            </motion.button>
           </Link>
         </motion.div>
       )}
--- pizzajoint/src/components/Toppings.js
+++ pizzajoint/src/components/Toppings.js
@@ -2,11 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden: {
+    x: "100vw",
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
+
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = ["mushrooms", "peppers", "onions", "olives", "extra cheese", "tomatoes"];
 
   return (
-    <div className="toppings container">
+    <motion.div className="toppings container" variants={containerVariants} initial="hidden" animate="visible">
       <h3>Step 2: Choose Toppings</h3>
       <ul>
         {toppings.map((topping) => {
@@ -20,9 +47,11 @@ const Toppings = ({ addTopping, pizza }) => {
       </ul>
 
       <Link to="/order">
-        <motion.button whileHover={{ scale: 1.1, textShadow: "0px 0px 8px rgb(255,255,255)", boxShadow: "0px 0px 8px rgb(255,255,255)" }}>Order</motion.button>
+        <motion.button variants={buttonVariants} whileHover="hover">
+          Order
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
