d5d556d62ae69bd8167fa406059b4081240c1749
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-14 23:17:00 +0700

    made modal with button perfetcly

--- pizzajoint/src/components/Modal.js
+++ pizzajoint/src/components/Modal.js
@@ -7,8 +7,33 @@ const backdrop = {
   hidden: { opacity: 0 },
 };
 
+const modal = {
+  hidden: {
+    opacity: 0,
+    y: "-100vh",
+  },
+  visible: {
+    opacity: 1,
+    y: "200px",
+    transition: { delay: 0.5 },
+  },
+};
+
 const Modal = ({ showModal, setShowModal }) => {
-  return <AnimatePresence exitBeforeEnter>{showModal && <motion.div className="backdrop" variants={backdrop} animate="visible" initial="hidden"></motion.div>}</AnimatePresence>;
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {showModal && (
+        <motion.div className="backdrop" variants={backdrop} animate="visible" initial="hidden" exit="hidden">
+          <motion.div className="modal" variants={modal}>
+            <p>Want to make another pizza ?</p>
+            <Link to="/">
+              <button>Order Again</button>
+            </Link>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Modal;
